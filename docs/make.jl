6699171623731f3f5057726c36eb9702d29678e4
Scott McIntire <scottrsm@gmail.com>
2024-04-03 13:59:14 -0400

    Added workflow information to run tests and create/deploy documentation

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,15 @@
-using Documenter, Boolean
+using Documenter
+using Boolean
 
-makedocs(sitename="Boolean Docs")
+makedocs(
+	sitename = "Boolean",
+	format = Documenter.HTML(),
+	modules = [Boolean]
+	)
 
+	# Documenter can also automatically deploy documentation to gh-pages.
+	# See "Hosting Documentation" and deploydocs() in the Documenter manual
+	# for more information.
+	deploydocs(
+		repo = "github.com/scottrsm/Boolean.jl.git"
+	)
